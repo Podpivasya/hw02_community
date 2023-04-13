9a5b65fa1625d9236a3342817fa0485a4ddb87f7
Анастасия Жибарева <anastasiy.azhibareva1@gmail.com>
2023-04-13 20:42:26 +0300

    flake problem

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,14 +2,14 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-User = get_user_model() 
+User = get_user_model()
 
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
- 
+    
     def __str__(self):
         return self.title
 
@@ -29,4 +29,3 @@ class Post(models.Model):
         null=True,
         related_name='posts'
         )
-
