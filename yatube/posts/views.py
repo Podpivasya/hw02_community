5021069f898c2c690f19b6befe871ddad7178202
Анастасия Жибарева <anastasiy.azhibareva1@gmail.com>
2023-04-13 20:30:11 +0300

    change admin

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
-def index(request):    
+def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
 
 # Страница со списком мороженого
