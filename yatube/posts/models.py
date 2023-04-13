38876651467300330bf2730c24d6e890d09f3a2b
Анастасия Жибарева <anastasiy.azhibareva1@gmail.com>
2023-04-13 20:56:51 +0300

    pep8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -9,7 +9,7 @@ class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    
+   
     def __str__(self):
         return self.title
 
@@ -28,4 +28,4 @@ class Post(models.Model):
         blank=True,
         null=True,
         related_name='posts'
-        )
+    )
